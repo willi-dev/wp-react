45b4c4344861ed085b0c904800d28d6cb1261153
willi-dev <willi.ilmukomputer@gmail.com>
2018-02-02 18:45:21 +0700

    update

--- wp-react-app/src/App.js
+++ wp-react-app/src/App.js
@@ -1,12 +1,33 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+	
+	constructor(){
+		super();
+		this.state = {
+			menu: []
+		};
+	}
+
+	componentWillMount(){
+		axios.get("http://localhost/_learn/wp-react/wordpress/wp-json/app/v1/menu")
+        .then(res => {
+        		console.log( res );
+            this.setState({
+                menu: res.data[0].menu_html, update: true
+            })
+        }); 
+    // console.log(this.state.menu)
+	}
   render() {
+  	const renderHTML = (rawHTML: string) => React.createElement("div", { dangerouslySetInnerHTML: { __html: rawHTML } });
+
     return (
       <div className="container">
         <div className="row">
-        	test aja
+        	{ renderHTML(this.state.menu) }
         </div>
       </div>
     );
